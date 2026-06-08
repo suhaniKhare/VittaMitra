b72d3798e832954f134c5fa8780c03c1bf510909
suhaniKhare <suhanikhare1345@gmail.com>
2026-06-09 01:51:02 +0530

    resolved-conflicts

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
-import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
 import ledgerRoutes from './routes/ledgerRoutes.js';
 
 dotenv.config();
